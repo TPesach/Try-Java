8b5d2795ef605596257e17e3ddddfd5c4be267b1
TPesach <tz8503973@gmail.com>
2023-07-06 16:22:46 +0300

    add some changes on: Game & Soldier

--- Game.java
+++ Game.java
@@ -1,3 +1,32 @@
 public class Game {
+    private Square[][] gameBoard;
+    private Soldier[] whiteSoldiers;
+    private Soldier[] blacksoldiers;
 
+    public Game(){
+
+    }
+    public Square[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    public Soldier[] getWhiteSoldiers() {
+        return whiteSoldiers;
+    }
+
+    public Soldier[] getBlacksoldiers() {
+        return blacksoldiers;
+    }
+
+    private void setGameBoard(Square[] gameBoard) {
+        this.gameBoard = new Square[8][8];
+    }
+
+    private void setWhiteSoldiers(Soldier[] whiteSoldiers) {
+        this.whiteSoldiers = new Soldier[16];
+    }
+
+    private void setBlacksoldiers(Soldier[] blacksoldiers) {
+        this.blacksoldiers = new Soldier[16];
+    }
 }
--- Soldier.java
+++ Soldier.java
@@ -1,6 +1,17 @@
 public abstract class Soldier {
     private String color;
-    private int X;
+    private int x;
     private int y;
     public abstract void validStep(int x,int y);
+
+    public abstract String getColor();
+
+    public abstract int getX();
+
+    public abstract int getY();
+
+    public abstract void setColor(String color);
+    public abstract void setX(int x);
+
+    public abstract void setY(int y);
 }
