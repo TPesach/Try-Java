dd736983032183879d0975feb65bb2df6c6fe1c3
TPesach <tz8503973@gmail.com>
2023-07-06 14:42:50 +0300

    add Square class

---
+++ Square.java
@@ -0,0 +1,29 @@
+public class Square {
+    private int x;
+    private int y;
+    private Soldier soldier;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Soldier getSoldier() {
+        return soldier;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setSoldier(Soldier soldier) {
+        this.soldier = soldier;
+    }
+}
